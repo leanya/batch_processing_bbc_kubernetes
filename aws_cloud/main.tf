92b711fe312d0433371509804d23bbba9a6e0005
LeanYinAi <yin_ai_lean@aiap.sg>
2025-11-17 15:52:10 +0800

    debug: check publc ip in provisioner

--- aws_cloud/main.tf
+++ aws_cloud/main.tf
@@ -39,10 +39,11 @@ resource "aws_instance" "myec2_tf" {
       "sudo swapoff -a",
       "sudo sed -i '/swap/d' /etc/fstab",
       "PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)",
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION='v1.28.6+k3s1' INSTALL_K3S_EXEC='--disable=traefik --tls-san $PUBLIC_IP' sh -",
+      "sudo curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION='v1.28.6+k3s1' INSTALL_K3S_EXEC='--disable=traefik --tls-san $PUBLIC_IP' sh -",
+      "echo PUBLIC_IP=$PUBLIC_IP",
       "sudo chmod 644 /etc/rancher/k3s/k3s.yaml",
       "sudo chown ec2-user:ec2-user /etc/rancher/k3s/k3s.yaml",
-      "touch /var/run/k3s-ready",
+      "sudo touch /var/run/k3s-ready",
       "echo 'Waiting for k3s to be ready...'",
       "while [ ! -f /var/run/k3s-ready ]; do sleep 5; done",
       "echo 'k3s is ready, init script complete.'"
