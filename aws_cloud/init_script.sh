becc3c13a07a7a8659d83415861b74fe5479c27c
LeanYinAi <yin_ai_lean@aiap.sg>
2025-11-17 15:40:21 +0800

    debug: include ec2 public ip to tls san via provisioner

--- aws_cloud/init_script.sh
+++ aws_cloud/init_script.sh
@@ -4,24 +4,14 @@ set -ex
 sudo yum update -y
 sudo yum install git -y
 
-sudo swapoff -a
-sudo sed -i '/swap/d' /etc/fstab
+# sudo swapoff -a
+# sudo sed -i '/swap/d' /etc/fstab
 # curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.28.6+k3s1" sh -
 
-# public_ip = curl -v http://169.254.169.254/latest/meta-data/public-ipv4
-
-for i in {1..30}; do
-  public_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-  if [[ -n "$public_ip" ]]; then
-    break
-  fi
-  sleep 1
-done
-
-curl -sfL https://get.k3s.io | \
-  INSTALL_K3S_VERSION="v1.28.6+k3s1" \
-  INSTALL_K3S_EXEC="--disable=traefik --tls-san ${public_ip}" \
-  sh -
+# curl -sfL https://get.k3s.io | \
+#   INSTALL_K3S_VERSION="v1.28.6+k3s1" \
+#   INSTALL_K3S_EXEC="--disable=traefik --tls-san ${public_ip}" \
+#   sh -
 
 # Wait until k3s service is active
 # echo "Waiting for k3s service to be active..."
@@ -37,7 +27,7 @@ curl -sfL https://get.k3s.io | \
 
 
 # update kubeconfig permissions
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-sudo chown ec2-user:ec2-user /etc/rancher/k3s/k3s.yaml
+# sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+# sudo chown ec2-user:ec2-user /etc/rancher/k3s/k3s.yaml
 
-touch /var/run/k3s-ready
+# touch /var/run/k3s-ready
--- aws_cloud/main.tf
+++ aws_cloud/main.tf
@@ -33,9 +33,16 @@ resource "aws_instance" "myec2_tf" {
     Name = "terraform_ec2"
   }
 
-  # provisioner to wait for k3s in the init-script
+  # provisioner to install and wait for k3s in the init-script
   provisioner "remote-exec" {
     inline = [
+      "sudo swapoff -a",
+      "sudo sed -i '/swap/d' /etc/fstab",
+      "PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)",
+      "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION='v1.28.6+k3s1' INSTALL_K3S_EXEC='--disable=traefik --tls-san $PUBLIC_IP' sh -",
+      "sudo chmod 644 /etc/rancher/k3s/k3s.yaml",
+      "sudo chown ec2-user:ec2-user /etc/rancher/k3s/k3s.yaml",
+      "touch /var/run/k3s-ready",
       "echo 'Waiting for k3s to be ready...'",
       "while [ ! -f /var/run/k3s-ready ]; do sleep 5; done",
       "echo 'k3s is ready, init script complete.'"
@@ -45,7 +52,7 @@ resource "aws_instance" "myec2_tf" {
       type        = "ssh"
       host        = self.public_ip
       user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")  # can use GitHub secret
+      private_key = file("~/.ssh/id_rsa") 
     }
   }
 }
